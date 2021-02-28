1246e7ba2f509cd6315329dce4a9df86777ed925
Karlik <kkkj91@yahoo.pl>
2021-02-28 19:07:34 +0100

    updated phpcs.xml.dist

--- app/src/Controller/TestController.php
+++ app/src/Controller/TestController.php
@@ -8,16 +8,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AppController extends AbstractController
+class TestController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $t = 1;
+        $t  = 1;
         $t2 = 2;
 
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
-        ]);
+        return $this->render(
+            'test/index.html.twig',
+            ['controller_name' => 'TestController']
+        );
     }
 }
---
+++ scripts/login_app.sh
@@ -0,0 +1,1 @@
+docker exec -it app.php bash
